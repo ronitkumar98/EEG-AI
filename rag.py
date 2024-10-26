2c295a2c0ebcbc465a11ed71ea5aeaa6539bfa6e
ronit <ronitkumar9874@gmail.com>
2024-10-27 02:14:56 +0530

    calling main

--- rag.py
+++ rag.py
@@ -200,4 +200,5 @@ async def chat_endpoint(question: str = Query(...)):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #uvicorn.run(app, host="0.0.0.0", port=8000)
+    main()
